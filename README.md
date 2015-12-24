55836356406cabbe477a4bd5726876378865e47c
Anthony DeVincenzi <tonyd@tonyd-mb.corp.dropbox.com>
2015-12-24 13:12:13 -0800

    Initial push
    Basic components for tip calculator demo

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tips Calculator Demo
+
+This is an iOS application demonstrating the basic functionality of a tip calculator. Math is hard, so there's an app for that!  See CodePath's ["Thanks for applying!"](http://courses.codepath.com/snippets/ios_for_designers/thanks_for_applying#heading-prework) page for more details.
+
+Time spent: 1.5 hours spent in total
+
+Completed user stories:
+
+ * [x] Required: User can input a total bill amount, in turn calculator the tip % and total amount to be paid
+ * [x] Required: User can toggle different percentages (18,20,22) via a segmented control to adjust the desired tip amount
+
+Walkthrough of all user stories:
+
+![Video Walkthrough](tipsdemo.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
---
+++ tips/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Tony DeVincenzi on 12/19/15.
+//  Copyright © 2015 Tony DeVincenzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tipLabel.text = "$0.00"
+        totalLabel.text = "0 .00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        print("User edited bill")
+        
+        var tipPercentages = [0.18, 0.20, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel .text = String(format: "$%.2f", tip)
+
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true  )
+    }
+
+}
+
